c1e68f3985079139c48b6b4418b5cfb52f94e5b0
Mostafizur06 <mostafizur1102@gmail.com>
2024-03-27 15:49:20 +0600

    cors

--- cookhub/settings.py
+++ cookhub/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://cookhub-django.onrender.com/','https://*.127.0.0.1']
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5500',
+]
 
 # Application definition
 
